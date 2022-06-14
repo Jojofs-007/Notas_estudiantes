4b83157e8f56fe15957a956e5dc3c55e1a4caad5
Jonathán Florián <73437429+Jojofs-007@users.noreply.github.com>
2022-06-14 00:29:20 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/notas/estudiantes/main.java
@@ -0,0 +1,10 @@
+package com.mycompany.notas.estudiantes;
+
+public class main {
+    public static void main(String[] args) {
+        alumno x = alumno.Estudiante;
+        x.CrearArchivo();
+        Principal z = new Principal();
+        z.setVisible(true);
+    } 
+}
